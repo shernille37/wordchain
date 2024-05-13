e3ebb18c6b3a5c77e7254b857c895dd5128aed34
Shernille Licud <shernillelicud@Shernilles-MacBook-Air.local>
2024-05-13 18:05:41 +0200

    Untrack files in .gitignore

--- file.h
+++
@@ -1,9 +0,0 @@
-void readFile(MapPrev * mp, char * fileName);
-void writeCsv(MapPrev *dict, char * filename);
-
-void STATE_1(int * state,wchar_t * word, int *i, wchar_t c, MapPrev * mp, wchar_t * prev, wchar_t ** firstWord, int *first);
-void STATE_2(int * state,wchar_t * word, int *i, wchar_t c, MapPrev * mp, wchar_t * prev, wchar_t ** firstWord, int *first);
-void STATE_3(int * state,wchar_t * word, int *i, wchar_t c, MapPrev * mp, wchar_t * prev, wchar_t ** firstWord, int *first);
-
-void readCsv(MapPrev *mp, char * filename);
-void produceText(MapPrev *mp, int nWords, char * prevWord);
