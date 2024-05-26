d1e9778f92ca77add44506e55abde6586e300126
Shernille Licud <shernillelicud@Shernilles-Air.Home>
2024-05-26 23:36:05 +0200

    Remove ignored files

--- multiProcess.h
+++
@@ -1,9 +0,0 @@
-#ifndef MULTIPROCESS_H
-#define MULTIPROCESS_H
-
-void multiCompito1(char * filename);
-
-void parentCompito1(int pipe1[2], char * fileName);
-void firstChildCompito1(int pipe1[2], int pipe2[2], char * fileName);
-
-#endif
